81649f6a48c057b33ed798f9a3f1244277b2a0ab
Faruq <lekside1@gmail.com>
2019-02-12 17:49:42 -0500

    Many changes. added printCards and createPowerPlantCards to the main and tested them.

--- COMP345Ass1/SummaryCards.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  SummaryCards.cpp
-//  COMP345Ass1
-//
-//  Created by Alek Faruq Aliu on 2019-02-08.
-//  Copyright © 2019 Adelekan Faruq Aliu. All rights reserved.
-//
-
-#include "SummaryCards.h"
