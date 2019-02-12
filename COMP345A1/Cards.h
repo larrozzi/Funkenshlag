397ac8c8b92279d180901e4185c6aaa738ebdef8
Faruq <lekside1@gmail.com>
2019-02-12 17:45:15 -0500

    added printCards and createPowerPlantCards to the main + tested the methods. added name attribute to SummaryCards class.

---
+++ COMP345A1/Cards.h
@@ -0,0 +1,18 @@
+//
+//  Cards.h
+//  COMP345A1
+//
+//  Created by Alek Faruq Aliu on 2019-02-10.
+//  Copyright © 2019 Adelekan Faruq Aliu. All rights reserved.
+//
+
+#pragma once
+
+#include <stdio.h>
+
+class Cards
+{
+public:
+    Cards();
+    ~Cards();
+};
---
+++ COMP345A1/Step3Card.cpp
@@ -0,0 +1,18 @@
+//
+//  Step3Card.cpp
+//  COMP345A1
+//
+//  Created by Alek Faruq Aliu on 2019-02-08.
+//  Copyright © 2019 Adelekan Faruq Aliu. All rights reserved.
+//
+
+#include "Step3Card.h"
+
+// constuctor
+Step3Card::Step3Card() {}
+Step3Card::~Step3Card() {}
+
+// setter/getter
+void Step3Card::setStep3Card(int step) { this->step = step; }
+inline int Step3Card::getStep3Card() const { return step; }
+
