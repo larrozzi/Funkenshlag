0df6139ab05fb0c2b8e7cd66181c38b11e0c5ff1
Unknown <jingtao1997@hotmail.com>
2019-03-05 17:48:31 -0500

    further work

--- FunkenshlagA1/MapLoader.cpp
+++ FunkenshlagA1/MapLoader.cpp
@@ -33,16 +33,18 @@ vector<CityNode> MapLoader::readMap(string f)
 // where all the magic happens
 vector<CityNode> MapLoader::exec()
 {
-	string cityString;
-	string edgesString;
-	string costString;
+	string cityString;	// city name
+	string edgesString;	//whole string with multiple edges
+	string costString;	//whole string with multiple costs
+	string line;	// individual line
+	string segment;
+	string edge;	//individual edge
+	string cost;	//individual cost
 	vector<string> edgesVector;
 	vector<string> costVector;
 	vector<CityNode> cities(100);
 	int i = 0;
 	bool used = true;
-	string line;
-	string segment;
 	ifstream myfile(this->fileName);
 	vector<int> costVectorInt;
 	vector<string> lineVariables(3);
@@ -52,22 +54,42 @@ vector<CityNode> MapLoader::exec()
 	
 		while (getline(myfile, line))	//takes a whole line
 		{
-			std::istringstream input;
-			input.str(line);
-			while (getline(input, segment,'|'))	//seperates line into segments divided by '|'
-			{
-				lineVariables.push_back(segment);
-			}
-			cityString = lineVariables.at(0);
-			edgesString = lineVariables.at(1);
-			costString = lineVariables.at(2);
+			istringstream linestream;
+			istringstream edgestream;
+			istringstream coststream;
 
-			boost::split(edgesVector, edgesString, [](char c) {return c == ','; });    //split edgesstring into vector delimiter: ','
-			boost::split(costVector, costString, [](char c) {return c == ','; });    // split coststring into vector delimiter: ','
+
+			linestream.str(line);
+			while (getline(linestream, segment,'|'))	//seperates line into segments divided by '|'
+			{
+				lineVariables.push_back(segment);	//pushes the segments into vector
+			}
+
+
+			cityString = lineVariables.at(0);		//assign the vector variables to corresponding Strings
+
+			edgesString = lineVariables.at(1);
+			edgestream.str(edgesString);			//convert edgesString to edgesstream to allow getline() use
+			while (getline(edgestream, edge, ','))
+			{
+				edgesVector.push_back(edge);	// each object in edgestream seperated by ',' is pushed into edgesVector
+			}
+			
+			costString = lineVariables.at(2);	//same stuff, but for cost
+			coststream.str(costString);
+			while (getline(coststream, cost, ','))
+			{
+				costVector.push_back(edge);
+			}
+
 			cities[i].setValues(cityString, edgesVector, costVector);
+			i++;
 		}
 		myfile.close();
 	}
 		else cout << "Incorrect file format";
 		return cities;
 }
+
+//boost::split(edgesVector, edgesString, [](char c) {return c == ','; });    //split edgesstring into vector delimiter: ','
+//boost::split(costVector, costString, [](char c) {return c == ','; });    // split coststring into vector delimiter: ','
