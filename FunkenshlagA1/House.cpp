58d1e136b7ee505edd5530cc22fcb86652fc7855
Anthony Menard-Gill <anthonymenardgill@gmail.com>
2019-03-06 10:27:50 -0500

    Put Function definition of Convert() in House.cpp, was in House.h

--- FunkenshlagA1/House.cpp
+++ FunkenshlagA1/House.cpp
@@ -22,6 +22,17 @@ void House::setColor(HouseColor color) {
 	this->color = color;
 }
 
+static HouseColor convert(const std::string& clr)
+{
+	if (clr == "NO_COLOR")		return NO_COLOR;
+	else if (clr == "RED")		return RED;
+	else if (clr == "BLUE")		return BLUE;
+	else if (clr == "GREEN")	return GREEN;
+	else if (clr == "YELLOW")	return YELLOW;
+	else if (clr == "BLACK")	return BLACK;
+	else if (clr == "PINK")		return PINK;
+	else return NO_COLOR;
+}
 // overloading output operator for the enum HouseColor
 std::ostream& operator<<(std::ostream& outs, const HouseColor& color) {
 	const char* c = 0;
