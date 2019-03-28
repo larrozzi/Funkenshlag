54da424fa6430ee7ade20345c7c88d7faf635589
Faruq <leksidealiu@hotmail.com>
2019-03-28 04:56:07 -0400

    phase 1-2 done

--- FunkenshlagA1/A2_Part3_Phase4_Driver.cpp
+++ FunkenshlagA1/A2_Part3_Phase4_Driver.cpp
@@ -1,36 +1,36 @@
-////
-////  A2_Part3_Phase4_Driver.cpp
-////  FunkenshlagA1
-////
-////  Created by Alek Faruq Aliu on 2019-03-24.
-////  Copyright © 2019 Adelekan Faruq Aliu. All rights reserved.
-////
-
-#include <stdio.h>
-#include <iostream>
-#include "Building.h"
-#include "Player.h"
-#include "MapLoader.h"
-#include "GameMap.h"
-
-
-int main()
-{
-    MapLoader mapLoader = MapLoader();
-    //GameMap gameMap = GameMap(mapLoader.readMap("map.txt"));
-    //gameMap.showMap(); // display map
-    
-    Building phase4 = Building();
-    std::cout << "Enter Number of players? \n> ";
-    int numberPlayers;
-	std::cin >> numberPlayers; 
-    phase4.NewGame(mapLoader, numberPlayers); // initialize players
-    
-    // memory thread problem ???
-    phase4.BeginPhase4();
-    phase4.Phase4BuyingCities();
-    phase4.EndPhase4();
-    
-	system("pause");
-    return 0 ;
-}
+//////
+//////  A2_Part3_Phase4_Driver.cpp
+//////  FunkenshlagA1
+//////
+//////  Created by Alek Faruq Aliu on 2019-03-24.
+//////  Copyright © 2019 Adelekan Faruq Aliu. All rights reserved.
+//////
+//
+//#include <stdio.h>
+//#include <iostream>
+//#include "Building.h"
+//#include "Player.h"
+//#include "MapLoader.h"
+//#include "GameMap.h"
+//
+//
+//int main()
+//{
+//    MapLoader mapLoader = MapLoader();
+//    //GameMap gameMap = GameMap(mapLoader.readMap("map.txt"));
+//    //gameMap.showMap(); // display map
+//
+//    Building phase4 = Building();
+//    std::cout << "Enter Number of players? \n> ";
+//    int numberPlayers;
+//    std::cin >> numberPlayers;
+//    phase4.NewGame(mapLoader, numberPlayers); // initialize players
+//
+//    // memory thread problem ???
+//    phase4.BeginPhase4();
+//    phase4.Phase4BuyingCities();
+//    phase4.EndPhase4();
+//
+//    system("pause");
+//    return 0 ;
+//}
